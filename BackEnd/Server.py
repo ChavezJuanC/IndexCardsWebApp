ec6c6573efadaa5ba2caa78375fd851a4e4192af
SKRPTZ <43616880+ChavezJuanC@users.noreply.github.com>
2025-01-31 17:52:21 -0600

    Implemtened Route to load local file as fetched data

--- BackEnd/Server.py
+++ BackEnd/Server.py
@@ -67,26 +67,45 @@ def delete_all_local_questions():
 ##Fourth route should be able to delete question by id.
 @app.delete("/local-questions/{id}")
 def delete_local_question(id: int):
-    with open("questions.json", mode="r", encoding="utf-8") as read_file:
-        question_data = json.load(read_file)
-        if not isinstance(question_data, list):
-            raise HTTPException(status_code=400, detail="Error Handling Data, Ensure Json is valid.")
+    try:
+        with open("questions.json", mode="r", encoding="utf-8") as read_file:
+            question_data = json.load(read_file)
+            if not isinstance(question_data, list):
+                raise HTTPException(status_code=400, detail="Error Handling Data, Ensure Json is valid.")
+        
+        starting_length : int = len(question_data)
+
+        if starting_length == 0:
+            raise HTTPException(status_code=400, detail="Empty Question File.")
+
+        question_data = [item for item in question_data if item.get("id") != id]
+
+        if (len(question_data) != starting_length):
+            with open("questions.json", mode="w", encoding="utf-8") as write_file:
+                json.dump(question_data, write_file)
+
+            return {"Message" : f"Question with id ({id}) has been deleted."}
+        
+        else:
+            raise HTTPException(status_code=404, detail=f"Question with id ({question_data}) not found.")
     
-    starting_length : int = len(question_data)
-
-    if starting_length == 0:
-        raise HTTPException(status_code=400, detail="Empty Question File.")
-
-    question_data = [item for item in question_data if item.get("id") != id]
-
-    if (len(question_data) != starting_length):
-        with open("questions.json", mode="w", encoding="utf-8") as write_file:
-            json.dump(question_data, write_file)
-
-        return {"Message" : f"Question with id ({id}) has been deleted."}
+    except(FileNotFoundError):
+        raise HTTPException(status_code=404, detail="No questions file found.")
     
-    else:
-        raise HTTPException(status_code=404, detail=f"Question with id ({question_data}) not found.")
+    except:
+        raise HTTPException(status_code=400, detail="Something went wrong while tryign to delete question.")
 
 ##Fifth route should be able to accept a exact file path and return all question in that json file..
-##Sixth route should be able to delete question from said file
+@app.get("/questions-file/{path}")
+def get_questions_from_filepath(path: str):
+
+    try:
+        with open(path, mode="r", encoding="utf-8") as read_file:
+            question_data = json.load(read_file)
+            if not isinstance(question_data, list):
+                question_data = []
+    
+    except (FileNotFoundError, json.JSONDecodeError):
+        question_data = []
+
+    return question_data
