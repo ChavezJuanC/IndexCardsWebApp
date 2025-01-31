58b7d43a54859ed535d1dd657cacb2ee9c76571f
SKRPTZ <43616880+ChavezJuanC@users.noreply.github.com>
2025-01-30 22:31:19 -0600

    Implemented routes to read from local file

--- BackEnd/Server.py
+++ BackEnd/Server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from Models import QuestionModel
 import json
 
@@ -22,10 +22,11 @@ async def new_question(question: QuestionModel.QuestionModel):
     try:
         with open("questions.json", mode="r", encoding="utf-8") as read_file:
             questions_data = json.load(read_file)
-            if not isinstance(questions_data, list):  # Ensure it's a list
+            if not isinstance(questions_data, list):  
                 questions_data = []
+                
     except (FileNotFoundError, json.JSONDecodeError):
-        questions_data = [] # Start fresh List
+        questions_data = [] 
 
     questions_data.append(question.to_dict())  
 
@@ -35,7 +36,34 @@ async def new_question(question: QuestionModel.QuestionModel):
     return {"Message" : "New Question Added"}
 
 ##Second route should be able to retrive all of the questions in local file...
+@app.get("/all-local-questions")
+def get_all_local_questions():
+    try:
+        with open("questions.json", mode="r", encoding="utf-8") as read_file:
+            question_data = json.load(read_file)
+            if not isinstance(question_data, list):
+                question_data = []
+    
+    except (FileNotFoundError, json.JSONDecodeError):
+        question_data = []
+
+    return question_data
+    
 ##Third route should be able to clear the entire local file...
+@app.delete("/all-local-questions")
+def delete_all_local_questions():
+    try:
+        with open("questions.json", mode="w", encoding="utf-8") as write_file:
+            json.dump("[]", write_file)
+
+            return{"Message" : "Question list was cleared"}
+
+    except(FileExistsError):
+        raise HTTPException(status_code=404, detail="File with local questions not found..")
+    
+    except:
+        raise HTTPException(status_code=400, detail="Error trying to read questions file.")
+
 ##Fourth route should be able to delete question by id.
 ##Fifth route should be able to accept a exact file path and return all question in that json file..
 ##Sixth route should be able to delete question from said file
